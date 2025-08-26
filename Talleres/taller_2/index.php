f4fc43478813dcb16e151ae172dfdaad10f64524
Arturo Phillips <arturophillipsl@gmail.com>
2025-08-25 19:53:34 -0500

    Definidas variables de diferentes tipos

--- Talleres/taller_2/index.php
+++ Talleres/taller_2/index.php
@@ -1,3 +1,14 @@
 <?php
+$nombre ="juan";
+$edad = 25;
+$altura = 1.80;
+$esEstudiante = true;
+
 echo "hello world";
+echo "nombre: $nombre<br>";
+echo "edad: $edad<br>";
+echo "altura: $altura<br>";
+echo "es estudiante? ". ($esEstudiante ? "si" : "no");
+
+
 ?>
