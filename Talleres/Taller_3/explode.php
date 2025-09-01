6b10b5108bac7db6357a3373c9f108cacfc69c77
Arturo Phillips <arturophillipsl@gmail.com>
2025-09-01 18:26:23 -0500

    añadido ejercicio de explode()

---
+++ Talleres/Taller_3/explode.php
@@ -0,0 +1,24 @@
+<?php
+// Ejemplo de uso de explode()
+$frase = "tralalero,tung_tung,ballerina,bombardiro";
+$frutas = explode(",", $frase);
+
+echo "Frase original: $frase</br>";
+echo "Array de frutas:</br>";
+print_r($frutas);
+
+
+$misPeliculas = "viernes_13-el_resplandor-pesadilla_de_la_calle_elm-el_conjuro";
+$arrayPeliculas = explode("-", $misPeliculas);
+
+echo "</br>Mis películas favoritas:</br>";
+print_r($arrayPeliculas);
+
+
+$texto = "mario,sonic,pikachu,samus,link;
+$array = explode(",", $texto, 3);
+
+echo "</br>Texto original: $texto</br>";
+echo "Array con límite:</br>";
+print_r($array);
+?>
